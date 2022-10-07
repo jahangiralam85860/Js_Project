55967ba253522a357cb9a154e668efff18e7d1e1
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-07 07:21:09 +0600

    submit

--- first.js
+++ first.js
@@ -3,3 +3,8 @@ var price = 10;
 var age = 20;
 var temperature = 38;
 console.log(price + age);
+
+var person = "jahangir alam";
+var location = "mirpur DOHS";
+var special = "programming";
+console.log(person + " " + location + " " + special);
