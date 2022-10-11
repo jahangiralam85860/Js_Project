4a4c552361f493bbf507f0c7dc24dca4d3c4b3f9
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-11 08:20:34 +0600

    sumbit

--- first.js
+++ first.js
@@ -43,3 +43,9 @@ var numberCheck = true;
 console.log(typeof numberCheck);
 var romantic;
 console.log(romantic);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1)
+console.log(sum);
