96d31d6ebd37ef042b7c39bffd9084d5020a94e7
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-12 09:28:46 +0600

    array length

--- array.js
+++ array.js
@@ -4,3 +4,4 @@ var picnicFee = [5000, 2000, 1000,2300,1400,1200];
 var nayikas = ['mahi', 'opu', 'shakib khan', 'sabnur', 'sabana', 'Hero alom'];
 var vowels = ['a','e','i','o,','u'];
 console.log(picnicFee.length);
+console.log(friendAge[2]);
