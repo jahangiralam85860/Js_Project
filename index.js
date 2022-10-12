57bc71618a3ccdff1dc7e301c9a15bd123e73e34
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-12 09:44:23 +0600

    index check

---
+++ index.js
@@ -0,0 +1,11 @@
+// array index
+var nayikas = ['mahi', 'opu', 'shakib khan', 'sabnur', 'sabana', 'Hero alom'];
+console.log(nayikas);
+// variable 
+var element = nayikas[1]
+console.log(element);
+nayikas[0] = 100;
+console.log(nayikas);
+//  index check
+var positionIndex = nayikas.indexOf("sabnur");
+console.log(positionIndex);
