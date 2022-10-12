a48718825e2bada79242dfe1db7e9f69247fedaf
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-13 00:35:05 +0600

    conditional

--- conditional.js
+++ conditional.js
@@ -1,5 +1,8 @@
 var iphone = 70000;
 var badujet = 60000;
-if (iphone ) {
-    
+if ( iphone < badujet ) {
+    console.log("I will pay iphone mobile");
+}
+else{
+    console.log("Amar taka nai ami walton phone kinbo");
 }
