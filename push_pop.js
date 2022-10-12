99b0202446a02bd31b72878730afeb77e2253f53
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-12 09:57:10 +0600

    push pop

---
+++ push_pop.js
@@ -0,0 +1,9 @@
+// array push, pop
+// push
+var nayikas = ['mahi', 'opu', 'shakib khan', 'sabnur', 'sabana', 'Hero alom'];
+nayikas.push('jahangir');
+// console.log(nayikas);
+
+// pop korle last element delet hobe
+nayikas.pop();
+console.log(nayikas);
