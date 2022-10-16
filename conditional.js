c9bd9f082ae0607c1ccd071250f190416b5c9a2a
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-17 00:25:16 +0600

    if else conditions add

--- conditional.js
+++ conditional.js
@@ -6,3 +6,27 @@ if ( iphone < badujet ) {
 else{
     console.log("Amar taka nai ami walton phone kinbo");
 }
+
+var goodBoy = true;
+var salary = 40000;
+var car = 1;
+if (goodBoy = false || salary > 50000 || car > 2) {
+ console.log("Hello friends");   
+}
+else{
+    console.log("kono conditon mill nai");
+}
+
+const arr = ['a', 'b', 'c'];
+const value1 = arr[Symbol.iterator]();
+
+
+console.dir( value1.next() );
+console.dir( value1.next() );
+console.dir( value1.next() );
+console.dir( value1.next() );
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
---
+++ if_else.js
@@ -0,0 +1,11 @@
+var money = 10;
+var butterBread = 35;
+var toastBiscuit = 20;
+var milk = 40;
+// condition add 
+if (butterBread < money) {
+    console.log("Ami akta danish khabo");
+}
+else if (butterBread > money) {
+    console.log("Amar Taka nai, so khawar kotha cinta korbo na");
+}
