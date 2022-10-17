a2015b499ace9c48b5e80206e3ef0fbc8fb1b87c
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-17 23:52:18 +0600

    array revision

--- revision.js
+++ revision.js
@@ -1,9 +1,21 @@
 // string type variable
 var bottle = 'water';
 var bottleColor = 'yellow';
+
 // number type variable
 var bottlePrice = 120;
 var salary = 15000;
+
 // boolean type variable
 var isExpensive = true;
 var notExpensive = false;
+
+var tableItems = ['bottle','mouse','pen','sunglass','paper'];
+// var items = tableItems[3];
+// tableItems[4] = "notebook"; 
+// var penIndex = tableItems.indexOf('pen');
+// tableItems.push('jahangir');
+// tableItems.pop();
+// tableItems.shift();
+// tableItems.unshift('hello');
+console.log(tableItems);
