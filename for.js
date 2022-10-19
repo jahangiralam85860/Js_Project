924c81a866b38b7ce2d9633c3600248aba3e5879
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-19 23:18:06 +0600

    string value print

--- for.js
+++ for.js
@@ -7,8 +7,13 @@
 // }
 
 var numbers = [10,20,30,40,50,60,70];
-for (let index = 0; index < 3; index++) {
+for (let index = 0; index < numbers.length; index++) {
     var number = numbers[index];
-    console.log(number);
-    
+    console.log(number); 
+}
+
+var names = ['jahangir','monirul','shofiq','hamid','masud','roky'];
+for (let i = 0; i < names.length; i++) {
+    const items = names[i];
+    console.log(items);
 }
