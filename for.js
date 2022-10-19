cedc48c3862a55b2ce132ea519bc7b84b926a9cb
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-19 22:39:49 +0600

    for

--- for.js
+++ for.js
@@ -1,3 +1,9 @@
 for (let index = 0; index < 10; index++) {
     console.log(index);
 } 
+
+for (let jahangir = 0; jahangir < 30; jahangir++) {
+    const element = array[jahangir];
+    console.log(element);
+    
+}
