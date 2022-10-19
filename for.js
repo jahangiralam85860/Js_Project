2be381ca5ffdd9b71afae162baa89f20a89e87aa
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-19 23:12:17 +0600

    for loop

--- for.js
+++ for.js
@@ -1,7 +1,14 @@
-for (let index = 0; index < 10; index++) {
-    console.log(index);
-} 
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+// } 
 
-for (let jahangir = 0; jahangir < 30; jahangir++) {
-    console.log(jahangir);
+// for (let jahangir = 0; jahangir < 30; jahangir++) {
+//     console.log(jahangir);
+// }
+
+var numbers = [10,20,30,40,50,60,70];
+for (let index = 0; index < 3; index++) {
+    var number = numbers[index];
+    console.log(number);
+    
 }
