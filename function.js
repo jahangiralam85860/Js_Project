047f43b807a2058733d43af86062507e995f8d56
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-29 23:15:09 +0600

    push

--- function.js
+++ function.js
@@ -4,3 +4,9 @@ function singara(money) {
 }
 var taka = 700;
 singara(taka);
+
+function add(number1, number2) {
+    console.log(number1, number2); 
+}
+var result = add(110,140);
+console.log(result);
