cf450033954829a4fb6c34e5b4dc31e40a3f9ec5
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-29 23:22:25 +0600

    pussh

--- function.js
+++ function.js
@@ -8,7 +8,8 @@ singara(taka);
 function add(number1, number2) {
     console.log(number1, number2);
     var sum = number1 + number2;
-    console.log(sum); 
+    // console.log(sum); 
+    return sum;
 }
-add(110,140);
-// console.log(result);
+var total = add(110,140);
+console.log('total=', total);
