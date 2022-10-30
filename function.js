905466c6e07a2361424d2b6ebaceafec1bf80e85
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-10-30 23:38:08 +0600

    object test

--- function.js
+++ function.js
@@ -12,4 +12,7 @@ function add(number1, number2) {
     return sum;
 }
 var total = add(110,140);
+var total = add(110,144);
 console.log('total=', total);
+
+var arrayObj = ['jahangir',{names: 'hero',age: '24',job: 'software'},Bangladesh];
